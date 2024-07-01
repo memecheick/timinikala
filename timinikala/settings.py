c49084ce2c391c7f88d97f460313c9085ea6ecbe
Badra <badra10alou10@gmail.com>
2024-07-01 14:08:16 +0000

    Media fix

--- timinikala/settings.py
+++ timinikala/settings.py
@@ -8,7 +8,8 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'main/media')
 
 
 # Quick-start development settings - unsuitable for production
